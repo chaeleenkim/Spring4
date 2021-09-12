b821a5fbf39342f587e9de804fe2ba1f4dfe8cf6
abcbcdyl <86539734+abcbcdy@users.noreply.github.com>
2021-09-12 20:47:22 +0900

    1

--- src/main/java/com/iu/s4/board/notice/NoticeService.java
+++ src/main/java/com/iu/s4/board/notice/NoticeService.java
@@ -27,6 +27,7 @@ public class NoticeService implements BoardService {
 	@Override
 	public BoardDTO getSelect(BoardDTO boardDTO) throws Exception {
 		// TODO Auto-generated method stub
+		noticeDAO.setHitUpdate(boardDTO);
 		return noticeDAO.getSelect(boardDTO);
 	}
 
